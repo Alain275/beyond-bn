5999b6e38df4f58f41237ae55d7fa214500283b4
Alain275 <alainshema275@gmail.com>
2026-03-20 20:49:29 +0200

    done

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ function createApp() {
   const defaultOrigins = [
     "http://localhost:5173",
     "https://beyond-bn.onrender.com/",
+    "https://beyondthehorizon-npc.netlify.app/",
     "http://localhost:5174",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
